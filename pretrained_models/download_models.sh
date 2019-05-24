41b86a1570fa10ce348f144611f621be90a6366d
Xiandong Meng <xmengab@connect.ust.hk>
2019-05-24 09:59:48 +0800

    Update download_models.sh

--- pretrained_models/download_models.sh
+++ pretrained_models/download_models.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-URL=http://vllab.ucmerced.edu/wlai24/video_consistency/models
-
-wget -N $URL/ECCV18_blind_consistency.pth
-wget -N $URL/ECCV18_blind_consistency_opts.pth
-wget -N $URL/FlowNet2_checkpoint.pth.tar
+URL=
